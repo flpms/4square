25b8ddb5a6cfbaaf32db9e82c71ca249c529a5a3
flpms <filipemelodasilva@gmail.com>
2015-10-31 17:45:07 -0200

    Files for tests and implementaion

---
+++ tests/lib/foursquare.test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var Foursquare = require('../../lib/foursquare.js');
+
+describe('Test for Foursquare Wrapper', function() {
+	
+	it('Expected deal with erros from get a token API')
+
+	it('Expected a token for Foursquare API');
+
+	it('Expected deal with erros with information');
+
+	it('Expected place information from Foursquare');
+
+	it('Expected deal with error in JSON response');
+
+	it('Expected a correct JSON from Foursquare')
+});
