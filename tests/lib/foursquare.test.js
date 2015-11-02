9dd51909299b82eb66552a3fe7c5b0cfccd1f455
flpms <filipemelodasilva@gmail.com>
2015-11-01 22:37:33 -0200

    test for new flow at public endpoints

--- tests/lib/foursquare.test.js
+++ tests/lib/foursquare.test.js
@@ -1,17 +1,27 @@
 var assert = require('assert');
 var Foursquare = require('../../lib/foursquare.js');
-
+console.log(Foursquare);
 describe('Test for Foursquare Wrapper', function() {
-	
-	it('Expected deal with erros from get a token API')
+    
+    it('Expected skyp OAuth flow for endpoints that don\'t OAuth Token', function() {
 
-	it('Expected a token for Foursquare API');
+        var foursquare = Foursquare.getResource({
+            clientId: 'R1QRCTI2BURVR1DFY5LZMG4WHCSOC5IKDSW1VVASR00DOLXU',
+            clientSecret: 'SV4CYDQOYL4CZRJNH555O31XPCTDLSQ4EURYYPKA51ZW2WQH'
+        });
 
-	it('Expected deal with erros with information');
+        assert.equal(typeof foursquare, 'object');
+    });
 
-	it('Expected place information from Foursquare');
+    it('Expected deal with erros from get a token API');
 
-	it('Expected deal with error in JSON response');
+    it('Expected a token for Foursquare API');
 
-	it('Expected a correct JSON from Foursquare')
+    it('Expected deal with erros with information');
+
+    it('Expected place information from Foursquare');
+
+    it('Expected deal with error in JSON response');
+
+    it('Expected a correct JSON from Foursquare')
 });
