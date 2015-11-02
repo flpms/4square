097c3bd7d81553323a5f741a856f3058da4b97b1
flpms <filipemelodasilva@gmail.com>
2015-11-02 21:53:01 -0200

    change tests to new flow

--- tests/lib/foursquare.test.js
+++ tests/lib/foursquare.test.js
@@ -1,27 +1,55 @@
 var assert = require('assert');
+var assertChai = require('chai').assert;
+var expect = require('chai').expect;
 var Foursquare = require('../../lib/foursquare.js');
-console.log(Foursquare);
-describe('Test for Foursquare Wrapper', function() {
-    
-    it('Expected skyp OAuth flow for endpoints that don\'t OAuth Token', function() {
 
-        var foursquare = Foursquare.getResource({
-            clientId: 'R1QRCTI2BURVR1DFY5LZMG4WHCSOC5IKDSW1VVASR00DOLXU',
-            clientSecret: 'SV4CYDQOYL4CZRJNH555O31XPCTDLSQ4EURYYPKA51ZW2WQH'
-        });
+ var oauthInfo = {
+    clientId: 'R1QRCTI2BURVR1DFY5LZMG4WHCSOC5IKDSW1VVASR00DOLXU',
+    clientSecret: 'SV4CYDQOYL4CZRJNH555O31XPCTDLSQ4EURYYPKA51ZW2WQH',
+    locale: 'pt'
+};
+
+describe('Test for Foursquare Wrapper', function() {
+
+    it('Call foursquare', function() {
+
+        var foursquare = Foursquare.init(oauthInfo);
 
         assert.equal(typeof foursquare, 'object');
+        expect(foursquare.data).to.have.property('clientSecret');
+        expect(foursquare.data).to.have.property('clientId');
+        expect(foursquare).to.have.property('search');
+        expect(foursquare).to.have.property('_get');
     });
 
-    it('Expected deal with erros from get a token API');
+    it('Expected search method in Foursquare object', function() {
 
-    it('Expected a token for Foursquare API');
+        var foursquare = Foursquare.init(oauthInfo);
 
-    it('Expected deal with erros with information');
+        assertChai.isFunction(foursquare.search);
+    });
 
-    it('Expected place information from Foursquare');
+    it('Expected a Venue from foursquare', function(done) {
 
-    it('Expected deal with error in JSON response');
+        this.timeout(20000);
 
-    it('Expected a correct JSON from Foursquare')
+        var foursquare = Foursquare.init(oauthInfo);
+
+        var callback = function(err, sucess) {
+            console.log(err);
+            console.log(sucess);
+
+            done();
+        };
+
+        var params =  {
+            limit: 10,
+            query: 'restaurantes',
+            near: 'Santo André, SP'
+        }
+
+        foursquare.search(params, 'venues', callback)     
+
+        assertChai
+    });
 });
