b1b48bade9a72292828593cd27749f706cfec4d3
flpms <filipemelodasilva@gmail.com>
2015-11-03 22:34:23 -0200

    More scenarios and deal better with erros

--- tests/lib/foursquare.test.js
+++ tests/lib/foursquare.test.js
@@ -29,27 +29,65 @@ describe('Test for Foursquare Wrapper', function() {
         assertChai.isFunction(foursquare.search);
     });
 
-    it('Expected a Venue from foursquare', function(done) {
+    it('Expected a list Venues from foursquare', function(done) {
 
-        this.timeout(20000);
+        this.timeout(30000);
 
         var foursquare = Foursquare.init(oauthInfo);
 
         var callback = function(err, sucess) {
-            console.log(err);
-            console.log(sucess);
+
+            expect(sucess).to.have.property('meta');
+            expect(sucess).to.have.property('response');
+            expect(sucess.meta).to.have.property('code');
+            expect(sucess.meta.code).to.be.equal(200);
+            expect(sucess.response).to.have.property('venues');
+            expect(sucess.response.venues).to.be.a('array');
+            expect(sucess.response.venues.length).to.equal(20);
 
             done();
         };
 
         var params =  {
-            limit: 10,
-            query: 'restaurantes',
+            limit: 20,
+            query: 'mazza restaurante',
             near: 'Santo André, SP'
-        }
+        };
 
-        foursquare.search(params, 'venues', callback)     
+        foursquare.search(params, 'venues', callback);
+    });
 
-        assertChai
+    it('Expected a error when search for venue from foursquare', function(done){
+        this.timeout(30000);
+
+        var foursquare = Foursquare.init(oauthInfo);
+
+        var callback = function(err, sucess) {
+
+            expect(err).to.have.property('meta');
+            expect(err.meta).to.have.property('code');
+            expect(err.meta.code).to.be.equal(400);
+
+            done();
+        };
+
+        foursquare.venue('4c20fd99ebe52d7fef7b3178', callback);
+    });
+
+    it('Expected a sucess when search for venue from foursquare', function(done){
+        this.timeout(30000);
+
+        var foursquare = Foursquare.init(oauthInfo);
+
+        var callback = function(err, sucess) {
+
+            expect(sucess).to.have.property('meta');
+            expect(sucess.meta).to.have.property('code');
+            expect(sucess.meta.code).to.be.equal(200);
+
+            done();
+        };
+
+        foursquare.venue('4c20fd99ebe52d7fef7b3178', callback);
     });
 });
